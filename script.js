7c45d8137fd5d181df97e56efaab86cda4daca70
ryanthayes <127283656+ryanthayes@users.noreply.github.com>
2023-10-09 22:00:36 -0700

    HTML markup and CSS styling

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.getElementById('equalsBtn')
+const clearButton = document.getElementById('clearBtn')
+const deleteButton = document.getElementById('deleteBtn')
+const pointButton = document.getElementById('pointBtn')
+
