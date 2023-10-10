3bf9ee085375281f4e6723b53d6521666596b0ec
ryanthayes <127283656+ryanthayes@users.noreply.github.com>
2023-10-09 22:18:25 -0700

    Add logic for basic math functions

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const numberButtons = document.querySelectorAll('[data-number]')
 const operatorButtons = document.querySelectorAll('[data-operator]')
-const equalsButton = document.getElementById('equalsBtn')
-const clearButton = document.getElementById('clearBtn')
-const deleteButton = document.getElementById('deleteBtn')
-const pointButton = document.getElementById('pointBtn')
+const equalsButton = document.getElementById('btn-equals')
+const clearButton = document.getElementById('btn-clear')
+const deleteButton = document.getElementById('btn-delete')
+const pointButton = document.getElementById('btn-point')
 
+const screenOutput = document.querySelector('screen-output');
+
+let a = 2;
+let b = 3;
+let firstNumber = a;
+let secondNumber = b;
+let operator = '';
+
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(`a + b = ${add(a, b)}`);
+console.log(`a - b = ${subtract(a, b)}`);
+console.log(`a * b = ${multiply(a, b)}`);
+console.log(`a / b = ${divide(a, b)}`);
+
+function operate(operator, firstNumber, secondNumber) {
+    subtract();
+}
+
+// Event listeners
+// numberButtons.addEventListener('click', function() {
+
+// });
